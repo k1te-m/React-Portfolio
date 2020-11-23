7ab677c7dc0faff76fec8070c8ca0b55e7f9bee6
k1te <kmiller343@gmail.com>
2020-11-23 15:13:48 -0600

    wrote navbar component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Nav = () => {
+
+return (
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">Kevin Miller</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/k1te-m" target="_blank">GitHub</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.linkedin.com/in/kmiller29/" target="_blank">LinkedIn</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://drive.google.com/file/d/1PJlN82yMOQarqMybW3sqvzhSk-l7f5mk/view"
+            target="_blank">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">About</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+}
