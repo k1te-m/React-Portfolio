fc0385092cde3f0233fd9ad52f4eb9157b3ac67c
k1te <kmiller343@gmail.com>
2020-11-23 15:31:17 -0600

    added switch route for home component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      <div>
         <Nav />
-      </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        
     </Router>
   );
 }
