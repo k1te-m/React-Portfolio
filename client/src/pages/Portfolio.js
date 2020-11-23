c8dc009e543912418cd22c3ee4506a3a48cb3a7c
k1te <kmiller343@gmail.com>
2020-11-23 15:55:19 -0600

    basic portfolio page JSX

---
+++ client/src/pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <div class="container">
+      <div class="row row-cols-3">
+        <div class="col">
+            {}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
