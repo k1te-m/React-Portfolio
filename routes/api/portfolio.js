73b87335258f365dd42b2a99a29d39b96510a254
k1te <kmiller343@gmail.com>
2020-11-23 23:56:06 -0600

    updated repoController import

--- routes/api/portfolio.js
+++ routes/api/portfolio.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const repoController = require("../../controllers/repoController");
+const repoController = require("../../controllers/reposController");
 
+
+// connects to /api/portfolio
 router
     .route("/")
     .get(repoController.findAll);
