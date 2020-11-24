a09a76bc60a97579490c19bf68b625239e0bfcb0
k1te <kmiller343@gmail.com>
2020-11-24 15:39:40 -0600

    added sendMessage to api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     getRepos: function() {
         return axios.get("/api/repos");
+    },
+    sendMessage: function() {
+        return axios.post("/api/message")
     }
 }
