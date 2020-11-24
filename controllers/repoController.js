028e37221d9a79138173a2c3d56b5faf4dc3d8b6
k1te <kmiller343@gmail.com>
2020-11-23 20:37:54 -0600

    updated findAll method

--- controllers/repoController.js
+++ controllers/repoController.js
@@ -2,6 +2,7 @@ const db = require("../models");
 
 module.exports = {
   findAll: function (req, res) {
+    console.log(req.query);
     db.Repo.find(req.query)
       .then((repos) => res.json(repos))
       .catch((error) => res.status(422).json(error));
