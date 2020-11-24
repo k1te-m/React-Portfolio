6000558dde829d4abef16360d92bad093b99ca96
k1te <kmiller343@gmail.com>
2020-11-24 01:15:09 -0600

    created state for repos, map state 4 each repoLink

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import API from "../utils/API";
+import RepoLink from "../components/RepoLink";
 
 function Portfolio() {
+  const [repoState, setRepoState] = useState([])
+  let repos = [];
 
   const getRepos = () => {
     API.getRepos().then(function(response) {
-      console.log(response);
+      repos = response.data;
+      setRepoState(repos);
     })
   }
   useEffect(() => {
     getRepos();
-  })
+  }, [])
+
+  console.log(repoState);
 
   return (
     <div className="container">
       <div className="row row-cols-3">
         <div className="col">
-            {}
+            {repoState.map(repo => {
+              return (
+                <RepoLink value={repo} key={repo._id}/>
+              )
+            })}
         </div>
       </div>
     </div>
