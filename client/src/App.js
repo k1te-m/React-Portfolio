ca3fcae2604aa7c54ec1f7b5697e0b5a2323683c
k1te <kmiller343@gmail.com>
2020-11-23 22:17:49 -0600

    Removed Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/portfolio" component={Portfolio} />
-        </Switch>   
-        <Footer />
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Footer />
     </Router>
   );
 }
