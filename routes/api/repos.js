6d5027df0ca6ff38fc933d1b82fc0f0a5a6c9d87
k1te <kmiller343@gmail.com>
2020-11-23 20:38:04 -0600

    removed generic route

--- routes/api/repos.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const repoController = require("../../controllers/repoController");
-
-router
-    .router("/")
-    .get(repoController.findAll);
-
-module.exports = router;
