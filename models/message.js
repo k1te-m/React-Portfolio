18207154cb83fb4c244b053866718cdce99f25de
k1te <kmiller343@gmail.com>
2020-11-24 15:39:53 -0600

    removed required from message schema

--- models/message.js
+++ models/message.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    message: { type: String, required: true}
+    name: { type: String },
+    email: { type: String },
+    message: { type: String }
 })
 
 const Message = mongoose.model("Message", messageSchema);
