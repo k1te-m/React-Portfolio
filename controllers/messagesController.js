0ac763fd37e81aab13d5d34855326b263a7fd910
k1te <kmiller343@gmail.com>
2020-11-24 18:05:07 -0600

    console.log added for testing

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -2,6 +2,7 @@ const db = require("../models");
 
 module.exports = {
   sendMessage: function (req, res) {
+    console.log(req.body);
     db.Message.create(req.body)
       .then((results) => {
         res.json(results);
