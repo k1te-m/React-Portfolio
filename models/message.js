d310c6a57eb8e518887e561c22f8c3e9f209ff46
k1te <kmiller343@gmail.com>
2020-11-24 19:48:21 -0600

    added date to message schema

--- models/message.js
+++ models/message.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
     name: { type: String },
     email: { type: String },
-    message: { type: String }
+    message: String,
+    date: { type: Date, default: Date.now }
 })
 
 const Message = mongoose.model("Message", messageSchema);
