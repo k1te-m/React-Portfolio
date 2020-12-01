85d19fee49a47954b1904a252f5fe74fa49be110
k1te <kmiller343@gmail.com>
2020-12-01 00:55:45 -0600

    updated repo links to include alt and noreferrer

--- client/src/components/RepoLink/index.js
+++ client/src/components/RepoLink/index.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const RepoLink = (props) => {
     return (
-        <a href={`https://www.github.com/${props.value.github}/${props.value.name}`} target="_blank"><img src={props.value.imageURL} className="repoImg mt-3 mb-1 ml-1 mr-1" /></a>
+        <a href={`https://www.github.com/${props.value.github}/${props.value.name}`} target="_blank" rel="noreferrer" ><img src={props.value.imageURL} alt={props.value.name} className="repoImg mt-3 mb-1 ml-1 mr-1" /></a>
     )
 }
 
