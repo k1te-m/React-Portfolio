f7621e0088aafc335677c737f11358a8d68baf2c
k1te <kmiller343@gmail.com>
2020-12-01 00:49:09 -0600

    updated contact page for modal functionality

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,38 +1,68 @@
 import React, { useState } from "react";
 import API from "../utils/API";
 import { Input, TextArea, FormBtn } from "../components/Form";
-
+import MessageModal from "../components/MessageModal";
 
 function Contact() {
   const [messageObject, setMessageObject] = useState({
     name: "",
     email: "",
-    message: ""
+    message: "",
+  });
+  const [messageModal, setMessageModal] = useState({
+    show: false,
+  });
+  const [displayOptions, setDisplayOptions] = useState({
+    display: "none",
+    zIndex: "2",
+    position: "fixed",
+    top: "20%",
+    left: "20%",
+    maxWidth: "50%",
+    height: "250px"
   })
 
   function handleInputChange(event) {
-    const { name, value } = event.target
-    setMessageObject({...messageObject, [name]: value})
+    const { name, value } = event.target;
+    setMessageObject({ ...messageObject, [name]: value });
+  }
+
+  function handleClose() {
+    setMessageModal(false);
+    setDisplayOptions({...displayOptions, display: "none"})
+  }
+  
+  function openModal(){
+    setMessageModal(true);
+    setDisplayOptions({...displayOptions, display: "block"})
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
     if (messageObject.name && messageObject.email && messageObject.message) {
-      console.log(messageObject);
       API.sendMessage({
         name: messageObject.name,
         email: messageObject.email,
-        message: messageObject.message
+        message: messageObject.message,
       })
         .then((res) => {
-          setMessageObject({
-            ...messageObject,
-            name: "",
-            email: "",
-            message: "",
-          });
+          if (res.status === 200) {
+            openModal();
+            setMessageObject({
+              ...messageObject,
+              name: "",
+              email: "",
+              message: "",
+            });
+          }
         })
         .catch((err) => console.log(err));
+    } else if (
+      !messageObject.name ||
+      !messageObject.email ||
+      !messageObject.message
+    ) {
+      alert("All fields are required.");
     }
   }
 
@@ -43,61 +73,61 @@ function Contact() {
           <h2 className="border-bottom pb-3 pt-3">Contact</h2>
           <form id="contactBox">
             <div className="form-row">
-              <Input 
+              <Input
                 onChange={handleInputChange}
                 name="name"
                 placeholder="Jane Smith"
                 value={messageObject.name}
               />
-              <Input 
+              <Input
                 onChange={handleInputChange}
                 name="email"
                 placeholder="jsmith@example.com"
                 value={messageObject.email}
               />
-              <TextArea 
+              <TextArea
                 onChange={handleInputChange}
                 name="message"
                 placeholder="Enter your message here!"
                 value={messageObject.message}
               />
-              <FormBtn
-                onClick={handleFormSubmit}
-              >
-                Submit
-              </FormBtn>
+              <FormBtn onClick={handleFormSubmit}>Submit</FormBtn>
             </div>
           </form>
-
-          <div className="modal" tabIndex="-1" id="messageModal" role="dialog">
-            <div className="modal-dialog" role="document">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h5>Thank You</h5>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    aria-label="Close"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <p>Your message has been received...</p>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-warning"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                </div>
+          <MessageModal show={messageModal}>
+            <div
+              className="modal container"
+              role="dialog"
+              id="messageModal"
+              style={displayOptions}
+            >
+              <div className="modal-header">
+                <h3>Thank you!</h3>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={handleClose}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <h5>Your message has been received. 😀</h5>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-warning"
+                  data-dismiss="modal"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
               </div>
             </div>
-          </div>
+          </MessageModal>
         </div>
       </div>
     </div>
